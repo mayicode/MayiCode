73d3411fd1767cc2b689b160bf8279d3f9bc5787
MayiCode <mayicode@163.com>
2017-07-25 11:49:02 +0800

    Update UTF-8_String_Cut.php

--- Tools-Class/UTF-8_String_Cut.php
+++ Tools-Class/UTF-8_String_Cut.php
@@ -1,34 +1,40 @@
 <?php
-
+/**
+ * @param $string 目标字符串
+ * @param $length 截取长度
+ * @param string $etc 是否带后...
+ * @return string
+ * 字符串截取
+ */
 public static function truncate_utf8_string($string, $length, $etc = '...')
+{
+	$result = '';
+	$string = html_entity_decode(trim(strip_tags($string)), ENT_QUOTES, 'UTF-8');
+	$strlen = strlen($string);
+	for ($i = 0; (($i < $strlen) && ($length > 0)); $i++)
 	{
-		$result = '';
-		$string = html_entity_decode(trim(strip_tags($string)), ENT_QUOTES, 'UTF-8');
-		$strlen = strlen($string);
-		for ($i = 0; (($i < $strlen) && ($length > 0)); $i++)
+		if ($number = strpos(str_pad(decbin(ord(substr($string, $i, 1))), 8, '0', STR_PAD_LEFT), '0'))
 		{
-			if ($number = strpos(str_pad(decbin(ord(substr($string, $i, 1))), 8, '0', STR_PAD_LEFT), '0'))
+			if ($length < 1.0)
 			{
-				if ($length < 1.0)
-				{
-					break;
-				}
-				$result .= substr($string, $i, $number);
-				$length -= 1.0;
-				$i += $number - 1;
+				break;
 			}
-			else
-			{
-				$result .= substr($string, $i, 1);
-				$length -= 0.5;
-			}
+			$result .= substr($string, $i, $number);
+			$length -= 1.0;
+			$i += $number - 1;
 		}
-		$result = htmlspecialchars($result, ENT_QUOTES, 'UTF-8');
-		if ($i < $strlen)
+		else
 		{
-			$result .= $etc;
+			$result .= substr($string, $i, 1);
+			$length -= 0.5;
 		}
-		return $result;
 	}
+	$result = htmlspecialchars($result, ENT_QUOTES, 'UTF-8');
+	if ($i < $strlen)
+	{
+		$result .= $etc;
+	}
+	return $result;
+}
  
 ?>
